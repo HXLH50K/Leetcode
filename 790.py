44c60104ea63d50ae932618cc2d125001e9297c0
hxlh50k <hxlh50k@email.com>
2022-11-12 23:26:16 +0800

    Daily quest 221112.

---
+++ 790.py
@@ -0,0 +1,18 @@
+# %%
+class Solution:
+    def f(self, x):
+        if x <= 2:
+            return x
+        if x == 3:
+            return 5
+        return (2 * self.f(x - 1) + self.f(x - 3)) % (10**9 + 7)
+
+    def numTilings(self, n: int) -> int:
+        res = [0, 1, 2, 5] + [0] * (n - 3)
+        for i in range(4, n + 1):
+            res[i] = (2 * res[i - 1] + res[i - 3]) % (10**9 + 7)
+        return res[n]
+
+
+Solution().numTilings(6)
+# %%
