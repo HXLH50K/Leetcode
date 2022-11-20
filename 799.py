adf4c924d3ee03b867c8e58d73a7ddff5abf45bd
hxlh50k <hxlh50k@email.com>
2022-11-20 17:13:04 +0800

    Daily quest 221120.

---
+++ 799.py
@@ -0,0 +1,13 @@
+# %%
+class Solution:
+    def champagneTower(self, poured: int, query_row: int, query_glass: int) -> float:
+        dp = [[0 for _ in range(101)] for _ in range(101)]
+        dp[0][0] = poured
+        for i in range(query_row + 1):
+            for j in range(i + 1):
+                if dp[i][j] >= 1:
+                    remain = dp[i][j] - 1
+                    dp[i][j] = 1
+                    dp[i + 1][j] += remain / 2
+                    dp[i + 1][j + 1] += remain / 2
+        return dp[query_row][query_glass]
