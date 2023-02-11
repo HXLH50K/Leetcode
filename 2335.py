4d07102cc9c9a3c688ef2252419ac0b26001456a
hxlh50k <hxlh50k@email.com>
2023-02-11 09:30:00 +0800

    Dailt quest 230211.

---
+++ 2335.py
@@ -0,0 +1,10 @@
+# %%
+from typing import List
+
+
+class Solution:
+    def fillCups(self, amount: List[int]) -> int:
+        amount.sort()
+        if amount[0] + amount[1] <= amount[2]:
+            return amount[2]
+        return (sum(amount) + 1) // 2
