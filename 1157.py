5cb5f4431a03910172c516455e705db22c48eb6c
hxlh50k <hxlh50k@gmail.com>
2023-04-16 10:13:19 +0800

    Daily quest 230416

---
+++ 1157.py
@@ -0,0 +1,16 @@
+# %%
+from collections import Counter
+from typing import List
+
+class MajorityChecker:
+
+    def __init__(self, arr: List[int]):
+
+
+    def query(self, left: int, right: int, threshold: int) -> int:
+
+
+
+# Your MajorityChecker object will be instantiated and called as such:
+# obj = MajorityChecker(arr)
+# param_1 = obj.query(left,right,threshold)
