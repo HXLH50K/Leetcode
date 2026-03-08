3cb1fa7ad5e1d179fab36180c98642abf5f3c6e9
hxlh50k <hxlh50k@gmail.com>
2026-03-08 10:36:53 +0800

    1980

---
+++ 1980.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def findDifferentBinaryString(self, nums: List[str]) -> str:
+        n = len(nums)
+        for i in range(2**n):
+            s = bin(i)[2:].zfill(n)
+            if s not in nums:
+                return s
+        return ""
