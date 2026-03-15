ef219fb3d9bf8f13aaf449b0fb4b4b58158c635b
hxlh50k <hxlh50k@gmail.com>
2026-03-15 10:34:36 +0800

    1622

---
+++ 1622.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+
+class Fancy:
+
+    def __init__(self):
+        self.arr = np.array([], dtype=np.int64)
+        self.MOD = 10**9 + 7
+
+    def append(self, val: int) -> None:
+        self.arr = np.append(self.arr, val)
+
+    def addAll(self, inc: int) -> None:
+        self.arr = (self.arr + inc) % self.MOD
+
+    def multAll(self, m: int) -> None:
+        self.arr = (self.arr * m) % self.MOD
+
+    def getIndex(self, idx: int) -> int:
+        return int(self.arr[idx]) if idx < len(self.arr) else -1
+
+
+# Your Fancy object will be instantiated and called as such:
+# obj = Fancy()
+# obj.append(val)
+# obj.addAll(inc)
+# obj.multAll(m)
+# param_4 = obj.getIndex(idx)
