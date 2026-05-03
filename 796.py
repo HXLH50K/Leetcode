9e11ec7f219cce921b5398dbcadb0f1f20c31dee
hxlh50k <hxlh50k@gmail.com>
2026-05-03 21:23:52 +0800

    796

---
+++ 796.py
@@ -0,0 +1,10 @@
+class Solution:
+    def rotateString(self, s: str, goal: str) -> bool:
+        m = len(s)
+        n = len(goal)
+        if m != n:
+            return False
+        for i in range(m):
+            if s[i:] + s[:i] == goal:
+                return True
+        return False
