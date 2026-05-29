fe2eda96e8aee56e8c3710b0e07c1b698efda935
hxlh50k <hxlh50k@gmail.com>
2026-05-29 18:48:45 +0800

    3300

---
+++ 3300.py
@@ -0,0 +1,7 @@
+from typing import List
+class Solution:
+    def minElement(self, nums: List[int]) -> int:
+        res = float('inf')
+        for num in nums:
+            res = min(res, sum(map(int, str(num))))
+        return res
