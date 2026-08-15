c33292fbce71cbd9c97a5ff7b876b79ad1b1f5de
hxlh50k <hxlh50k@gmail.com>
2026-08-15 08:35:13 +0800

    3702

---
+++ 3702.py
@@ -0,0 +1,11 @@
+from typing import List
+class Solution:
+    def longestSubsequence(self, nums: List[int]) -> int:
+        xor = 0
+        for num in nums:
+            xor ^= num
+        if xor != 0:
+            return len(nums)
+        if all(num == 0 for num in nums):
+            return 0
+        return len(nums) - 1
